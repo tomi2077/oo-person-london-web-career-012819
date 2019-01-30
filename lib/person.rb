d96b9126a9e146bbf7ddb3bc50b3cfd740fa0bff
Tominiyi Ajayi <tominiyimunito@Tominiyis-MacBook-Air.local>
2019-01-30 15:09:37 +0000

    Not Complete

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,56 @@
-# your code goes here
+class Person
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8
+    @hygiene = 8
+  end
+
+  def bank_account
+    @bank_account
+  end
+
+  def happiness
+    @happiness
+  end
+
+  def hygeine
+    @hygeine
+  end
+
+  def clean?
+    if hygiene > 7
+      return true
+    else
+      false
+    end
+  end
+
+  def happy?
+    if happiness > 7
+      return true
+    else
+      false
+    end
+  end
+
+  def get_paid(salary)
+    bank_account += salary
+    return "all about the benjamins"
+  end
+
+  def take_bath
+    hygiene += 4
+    return "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    happiness += 2
+    hygiene -= 3
+    return "♪ another one bites the dust ♫"
+  end
+
+end
