6a86af3adee24392237b355b84aaa92b16ec4892
Tominiyi Ajayi <tominiyimunito@Tominiyis-MacBook-Air.local>
2019-01-31 09:50:11 +0000

    OO Pairing Lab Completed

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,6 @@
 class Person
-
-  attr_accessor :name
+  attr_accessor :bank_account, :happiness, :hygiene
+  attr_reader :name
 
   def initialize(name)
     @name = name
@@ -9,48 +9,66 @@ class Person
     @hygiene = 8
   end
 
-  def bank_account
-    @bank_account
+  def happiness=(num)
+    @happiness = num
+    @happiness = 10 if @happiness > 10
+    @happiness = 0 if @happiness < 0
   end
 
-  def happiness
-    @happiness
-  end
-
-  def hygeine
-    @hygeine
-  end
-
-  def clean?
-    if hygiene > 7
-      return true
-    else
-      false
-    end
+  def hygiene=(num)
+    @hygiene = num
+    @hygiene = 10 if @hygiene > 10
+    @hygiene = 0  if @hygiene < 0
   end
 
   def happy?
-    if happiness > 7
+    if @happiness > 7
       return true
     else
-      false
+      return false
+    end
+
+  end
+
+  def clean?
+    if @hygiene > 7
+      return true
+    else
+      return false
     end
   end
 
   def get_paid(salary)
-    bank_account += salary
+    @bank_account += salary
     return "all about the benjamins"
   end
 
   def take_bath
-    hygiene += 4
+    self.hygiene += 4
     return "♪ Rub-a-dub just relaxing in the tub ♫"
   end
 
   def work_out
-    happiness += 2
-    hygiene -= 3
+    self.happiness += 2
+    self.hygiene -= 3
     return "♪ another one bites the dust ♫"
   end
 
+  def call_friend(friend)
+    friend.happiness += 3
+    self.happiness += 3
+    return "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
+
+  def start_conversation(person,topic)
+    if topic == "politics"
+      [person, self].each { |p| p.happiness -=2}
+      return "blah blah partisan blah lobbyist"
+    elsif topic == "weather"
+      [person,self].each {|p| p.happiness +=1}
+      return "blah blah sun blah rain"
+    else
+      return "blah blah blah blah blah"
+    end
+  end
 end
